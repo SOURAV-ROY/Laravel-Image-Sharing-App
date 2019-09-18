a68d52b998205e86d6c3d938920d10746640757a
SOURAV ROY <souravroy.info13@gmail.com>
2019-09-19 00:20:10 +0600

    Profile IMG Set

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Profile Home Page
                 </div>
 
                 <div class="links">
