73299ce4edac63636128eea16d73ff637d64dc76
SOURAV ROY <souravroy.info13@gmail.com>
2019-09-23 13:26:51 +0600

    Post View With name And UserName

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,7 +3,17 @@
 @section('content')
 <div class="container">
 
-<img src="/storage/{{$post->image}}">
-
+    <div class="row">
+        <div class="col-8">
+            <img src="/storage/{{$post->image}}" class="w-100">
+        </div>
+        <div class="col-4">
+            {{-- <h3>{{ $post->user->username }}</h3> --}}
+            <h3>{{ $post->user->name }}</h3>
+            <h5>{{ $post->user->username }}</h5>
+            <p>{{$post->caption}}</p>
+        </div>
+    </div>
+    
 </div>
 @endsection
