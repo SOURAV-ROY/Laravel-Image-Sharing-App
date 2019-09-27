b4d0e4a4fc736a03108277fdcf0825da82b93cb3
SOURAV ROY <souravroy.info13@gmail.com>
2019-09-27 12:32:04 +0600

    Folow Added

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -15,14 +15,26 @@
                         <img src="/storage/{{ $post->user->profile->image }}" class="rounded-circle w-100" style="max-width: 40px">
                     </div>
                     <div>
-                        <div class="font-weight-bold"><a href="/profile/{{ $post->user->id }}">{{ $post->user->name }}</a></div>
+                        <div class="font-weight-bold">
+                            <a href="/profile/{{ $post->user->id }}">
+                               <span class="text-info">{{ $post->user->name }}</span>
+                            </a> |
+                            <a href="#" class="pl-3 text-danger">Follow</a>
+                        </div>
                     </div>
                 </div>
-                <hr>
-            </div>
-                <p><span class="font-weight-bold"><a href="/profile/{{ $post->user->id }}">{{ $post->user->username }}</a></span> {{$post->caption}} </p>
-        </div>
 
+                <hr>
+
+            </div>
+                <p>
+                    <span class="font-weight-bold">
+                        <a href="/profile/{{ $post->user->id }}">
+                            <span class="text-success pr-2">{{ $post->user->username }} >> </span>
+                        </a>
+                    </span> {{$post->caption}}
+                </p>
+        </div>
     </div>
 
 </div>
