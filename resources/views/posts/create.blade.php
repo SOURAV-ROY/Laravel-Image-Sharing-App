d0e0ed15a6552f6ae0d83ddf68aa6e6d47d8bc20
SOURAV ROY <souravroy.info13@gmail.com>
2019-11-05 14:40:37 +0600

    PostCreate Reformat Code

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <form action="/p" enctype="multipart/form-data" method="POST">
-        @csrf
+    <div class="container">
+        <form action="/p" enctype="multipart/form-data" method="POST">
+            @csrf
             <div class="row">
                 <div class="col-8 offset-2">
                     <div>
@@ -12,33 +12,33 @@
 
                     <div class="form-group row">
                         <label for="caption" class="col-md-4 col-form-label">Post Caption</label>
-        
-                            <input id="caption"
-                                type="text" 
-                                class="form-control @error('caption') is-invalid @enderror" 
-                                name="caption" value="{{ old('caption') }}" 
-                                autocomplete="caption" autofocus >
-        
-                            {{-- @error('caption')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror --}}
 
-{{--########################### WoW Thing ****************************** --}}
-                            @if  ($errors->has('caption'))
-                                <strong>{{ $errors->first('caption') }}</strong>
-                            @endif
-{{--########################### WoW Thing ****************************** --}}
+                        <input id="caption"
+                               type="text"
+                               class="form-control @error('caption') is-invalid @enderror"
+                               name="caption" value="{{ old('caption') }}"
+                               autocomplete="caption" autofocus>
+
+                        {{-- @error('caption')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror --}}
+
+                        {{--########################### WoW Thing ****************************** --}}
+                        @if  ($errors->has('caption'))
+                            <strong>{{ $errors->first('caption') }}</strong>
+                        @endif
+                        {{--########################### WoW Thing ****************************** --}}
                     </div>
 
                     <div class="row">
-                            <label for="image" class="col-md-4 col-form-label">Post Image</label>
-                        <input type="file", class="form-control-file" id="image" name="image">
-                    
-                         @if  ($errors->has('image'))
-                                    <strong>{{ $errors->first('image') }}</strong>
-                        @endif                   
+                        <label for="image" class="col-md-4 col-form-label">Post Image</label>
+                        <input type="file" , class="form-control-file" id="image" name="image">
+
+                        @if  ($errors->has('image'))
+                            <strong>{{ $errors->first('image') }}</strong>
+                        @endif
 
                     </div>
 
@@ -48,6 +48,6 @@
 
                 </div>
             </div>
-    </form>
-</div>
+        </form>
+    </div>
 @endsection
