3b9f2fc4276bcd4fd393e9ad47eb1e841062a59e
SOURAV ROY <souravroy.info13@gmail.com>
2019-11-05 14:38:20 +0600

    Index Reformat Code

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,47 +1,48 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+    <div class="container">
 
-    @foreach($posts as $post)
+        @foreach($posts as $post)
 
-    <div class="row">
+            <div class="row">
 
-        <div class="col-6 offset-3">
-            <div>
-                <div class="d-flex align-items-center">
-                    <div class="pr-3 pt-3 pb-3">
-                        <img src="{{ $post->user->profile->profileImage() }}" class="rounded-circle w-100" style="max-width: 40px">
-                    </div>
+                <div class="col-6 offset-3">
                     <div>
-                        <div class="font-weight-bold">
-                            <a href="/profile/{{ $post->user->id }}">
-                               <span class="text-info">{{ $post->user->name }}</span>
-                            </a>
+                        <div class="d-flex align-items-center">
+                            <div class="pr-3 pt-3 pb-3">
+                                <img src="{{ $post->user->profile->profileImage() }}" class="rounded-circle w-100"
+                                     style="max-width: 40px">
+                            </div>
+                            <div>
+                                <div class="font-weight-bold">
+                                    <a href="/profile/{{ $post->user->id }}">
+                                        <span class="text-info">{{ $post->user->name }}</span>
+                                    </a>
+                                </div>
+                            </div>
                         </div>
                     </div>
+                    <p>
+                        <span class="font-weight-bold">{{$post->caption}}</span>
+                    </p>
                 </div>
+
+                <div class="col-6 offset-3" style="border: 1px solid green">
+                    <a href="/profile/{{ $post->user->id }}">
+                        <img src="/storage/{{$post->image}}" class="w-100">
+                    </a>
+                </div>
+
             </div>
-            <p>
-                <span class="font-weight-bold">{{$post->caption}}</span>
-            </p>
-        </div>
 
-        <div class="col-6 offset-3" style="border: 1px solid green">
-            <a href="/profile/{{ $post->user->id }}">
-                <img src="/storage/{{$post->image}}" class="w-100">
-            </a>
-        </div>
+        @endforeach
 
-    </div>
-
-    @endforeach
-
-    <div class="row pt-3">
-        <div class="col-12 d-flex justify-content-center">
+        <div class="row pt-3">
+            <div class="col-12 d-flex justify-content-center">
                 {{ $posts->links() }}
+            </div>
         </div>
-    </div>
 
-</div>
+    </div>
 @endsection
