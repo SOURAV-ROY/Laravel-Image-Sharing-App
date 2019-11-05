6dd3794285e60c9cd19bbffba9573b8a4a409c49
SOURAV ROY <souravroy.info13@gmail.com>
2019-11-05 14:42:26 +0600

    PostShow Reformat Code

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,32 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+    <div class="container">
 
-    <div class="row">
-        <div class="col-8">
-            <img src="/storage/{{$post->image}}" class="w-100">
-        </div>
+        <div class="row">
+            <div class="col-8">
+                <img src="/storage/{{$post->image}}" class="w-100">
+            </div>
 
-        <div class="col-4">
-            <div>
-                <div class="d-flex align-items-center">
-                    <div class="pr-3">
-                        <img src="{{ $post->user->profile->profileImage() }}" class="rounded-circle w-100" style="max-width: 40px">
-                    </div>
-                    <div>
-                        <div class="font-weight-bold">
-                            <a href="/profile/{{ $post->user->id }}">
-                               <span class="text-info">{{ $post->user->name }}</span>
-                            </a> |
-                            <a href="#" class="pl-3 text-danger">Follow</a>
+            <div class="col-4">
+                <div>
+                    <div class="d-flex align-items-center">
+                        <div class="pr-3">
+                            <img src="{{ $post->user->profile->profileImage() }}" class="rounded-circle w-100"
+                                 style="max-width: 40px">
+                        </div>
+                        <div>
+                            <div class="font-weight-bold">
+                                <a href="/profile/{{ $post->user->id }}">
+                                    <span class="text-info">{{ $post->user->name }}</span>
+                                </a> |
+                                <a href="#" class="pl-3 text-danger">Follow</a>
+                            </div>
                         </div>
                     </div>
+
+                    <hr>
+
                 </div>
-
-                <hr>
-
-            </div>
                 <p>
                     <span class="font-weight-bold">
                         <a href="/profile/{{ $post->user->id }}">
@@ -34,8 +35,8 @@
                         </a>
                     </span> {{$post->caption}}
                 </p>
+            </div>
         </div>
-    </div>
 
-</div>
+    </div>
 @endsection
