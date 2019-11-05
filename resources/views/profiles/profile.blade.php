b0e700ea79f9eb48fe1a0da87bcc2d7fe3a3b82a
SOURAV ROY <souravroy.info13@gmail.com>
2019-11-05 14:34:24 +0600

    Profile View Modify

--- resources/views/profiles/profile.blade.php
+++ resources/views/profiles/profile.blade.php
@@ -1,28 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+    <div class="container">
 
-    <div class="row">
-        <div class="col-3 p-5">
-{{--        <img src="https://avatars0.githubusercontent.com/u/8663561?s=460&v=4" style=" width:150px; height:150px;" class="rounded-circle">--}}
-            <img src="{{ $user->profile->profileImage() }}" class="rounded-circle w-100">
-        </div>
-
-        <div class="col-9 pt-5">
-            {{-- <div><h1>{{ $user->username }}</h1></div> --}}
-            <div class="d-flex justify-content-between align-items-baseline">
-
-                <div class="d-flex align-items-center pb-3">
-                    <div class="h4">{{ $user->name }}</div>
-{{--                        <button class="btn btn-primary ml-4">FOLLOW</button>--}}
-                    <follow-button user-id="{{ $user->id }}" follows="{{ $follows }}"></follow-button>
-                </div>
-
-                @can('update', $user->profile)
-                    <button class="btn btn-outline-success"><a href="/p/create">Add New Post</a></button>
-                @endcan
+        <div class="row">
+            <div class="col-3 p-5">
+                {{--        <img src="https://avatars0.githubusercontent.com/u/8663561?s=460&v=4" style=" width:150px; height:150px;" class="rounded-circle">--}}
+                <img src="{{ $user->profile->profileImage() }}" class="rounded-circle w-100">
             </div>
+
+            <div class="col-9 pt-5">
+                {{-- <div><h1>{{ $user->username }}</h1></div> --}}
+                <div class="d-flex justify-content-between align-items-baseline">
+
+                    <div class="d-flex align-items-center pb-3">
+                        <div class="h4">{{ $user->name }}</div>
+                        {{--                        <button class="btn btn-primary ml-4">FOLLOW</button>--}}
+                        <follow-button user-id="{{ $user->id }}" follows="{{ $follows }}"></follow-button>
+                    </div>
+
+                    @can('update', $user->profile)
+                        <button class="btn btn-outline-success"><a href="/p/create">Add New Post</a></button>
+                    @endcan
+                </div>
                 @can('update', $user->profile)
                     <a href="/profile/{{ $user->id }}/edit">Edit Profile</a>
                 @endcan
@@ -39,39 +39,39 @@
                     </div>
                 </div>
 
-           <!--  <div class="pt-4 font-weight-bold">Sourav Roy</div>
-            <div>
-                Love Travels ❤🛫 <br> Life Is Too Short 😥 So Enjoy The Life 😎😍
-            </div>
-            <div>
-                <a href="">https://www.facebook.com/SOURAVROY.CSE</a>
-            </div> -->
+                <!--  <div class="pt-4 font-weight-bold">Sourav Roy</div>
+                 <div>
+                     Love Travels ❤🛫 <br> Life Is Too Short 😥 So Enjoy The Life 😎😍
+                 </div>
+                 <div>
+                     <a href="">https://www.facebook.com/SOURAVROY.CSE</a>
+                 </div> -->
 
-            <div class="pt-4 font-weight-bold">
-                {{$user->profile->title ?? 'N/A'}}
-            </div>
-            <div>
-                {{$user->profile->description ?? 'N/A'}}
-            </div>
+                <div class="pt-4 font-weight-bold">
+                    {{$user->profile->title ?? 'N/A'}}
+                </div>
+                <div>
+                    {{$user->profile->description ?? 'N/A'}}
+                </div>
 
-            <div>
-                <a href="#">{{$user->profile->url ?? 'N/A'}}</a>
-            </div>
+                <div>
+                    <a href="#">{{$user->profile->url ?? 'N/A'}}</a>
+                </div>
 
+            </div>
         </div>
-    </div>
 
-    <div  class="row pt-5">
-{{--********************************** Image View ***********************************--}}
+        <div class="row pt-5">
+        {{--********************************** Image View ***********************************--}}
         @foreach ($user->posts as $post)
-        <!-- <div style="border: 1px solid darkgreen" class="col-4 pb-4"> -->
-        <div class="col-4 pb-4" style="border: 1px solid red">
-            <a href="/p/{{ $post->id }}">
-                 <img src="/storage/{{ $post->image }}"class="w-100">
-            </a>
+            <!-- <div style="border: 1px solid darkgreen" class="col-4 pb-4"> -->
+                <div class="col-4 pb-4" style="border: 1px solid red">
+                    <a href="/p/{{ $post->id }}">
+                        <img src="/storage/{{ $post->image }}" class="w-100">
+                    </a>
+                </div>
+            @endforeach
         </div>
-        @endforeach
-    </div>
 
-</div>
+    </div>
 @endsection
